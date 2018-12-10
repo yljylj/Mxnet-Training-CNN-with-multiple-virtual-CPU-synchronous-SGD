576aee8a560173792aa709ff0534fb6380cd25cb
yljylj <925980313@qq.com>
2018-12-10 10:36:19 +0800

    Update and rename example/image_classification/run.sh to run.sh

--- example/image_classification/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export OMP_NUM_THREADS=7
-rm out.txt out.err
-export MXNET_ENGINE_TYPE=NaiveEngine
-nohup taskset -c 0-6 python train_imagenet.py --work-id=0 1>>out.txt 2>>out.err &
-sleep 2
-nohup taskset -c 7-13 python train_imagenet.py --work-id=1 1>>out.txt 2>>out.err &
-nohup taskset -c 14-20 python train_imagenet.py --work-id=2 1>>out.txt 2>>out.err &
-nohup taskset -c 21-27 python train_imagenet.py --work-id=3 1>>out.txt 2>>out.err &
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export OMP_NUM_THREADS=7
+rm out.txt out.err
+export MXNET_ENGINE_TYPE=NaiveEngine
+nohup taskset -c 0-6 python example/imaget_classification/train_imagenet.py --work-id=0 1>>out.txt 2>>out.err &
+sleep 2
+nohup taskset -c 7-13 python example/imaget_classification/train_imagenet.py --work-id=1 1>>out.txt 2>>out.err &
+nohup taskset -c 14-20 python example/imaget_classification/train_imagenet.py --work-id=2 1>>out.txt 2>>out.err &
+nohup taskset -c 21-27 python example/imaget_classification/train_imagenet.py --work-id=3 1>>out.txt 2>>out.err &
