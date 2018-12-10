2aec26b8952af70e987a520ae5dd168fe1e7f48d
yljylj <925980313@qq.com>
2018-12-10 10:35:21 +0800

    Update and rename run.sh to example/image_classification/run.sh

--- example/image_classification/run.sh
+++ example/image_classification/run.sh
@@ -7,7 +7,3 @@ sleep 2
 nohup taskset -c 7-13 python train_imagenet.py --work-id=1 1>>out.txt 2>>out.err &
 nohup taskset -c 14-20 python train_imagenet.py --work-id=2 1>>out.txt 2>>out.err &
 nohup taskset -c 21-27 python train_imagenet.py --work-id=3 1>>out.txt 2>>out.err &
-#nohup taskset -c 28-34 python train_imagenet.py --work-id=4 1>>out.txt 2>>out.err &
-#nohup taskset -c 35-41 python train_imagenet.py --work-id=5 1>>out.txt 2>>out.err &
-#nohup taskset -c 42-48 python train_imagenet.py --work-id=6 1>>out.txt 2>>out.err &
-#nohup taskset -c 49-55 python train_imagenet.py --work-id=7 1>>out.txt 2>>out.err &
